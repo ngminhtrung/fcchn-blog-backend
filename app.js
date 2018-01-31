3b8ca0a0e2a3a86762d32b83a1e4ee237d187616
tungvu3196 <tungvu3196@gmail.com>
2018-01-31 20:54:07 +0700

    add console.log

--- app.js
+++ app.js
@@ -31,7 +31,8 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   res.status(err.status || 500);
-  res.send(err);
+  console.log(err);
+  res.send(err); 
 });
 
 // plugin bluebird promise in mongoose
