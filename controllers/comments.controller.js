0ffc162462f05d9decd25a92835f204227b91611
Trung Nguyen <minhtrung.nguyen@live.com>
2018-01-31 17:44:27 +0700

    edit CRUD comments

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -1,57 +1,30 @@
 import Comment from '../models/comments.model';
-import bcrypt from 'bcrypt';
 
 /**
- * Load comment and append to req.
+ * Load all comments of post 
  */
-function load(req, res, next, id) {
-  Comment.get(id)
-    .then((comment) => {
-      req.comment = comment; // eslint-disable-line no-param-reassign
-      return next();
-    })
-    .catch(e => next(e));
+
+exports.index = function(req, res) {
+  res.send('Not IMPLEMENTED: List of comments')
+}
+ /**
+  * Create a new comment for a post
+  */
+
+exports.create = function(req, res) {
+    res.send('Not IMPLEMENTED: Comment create POST')
+  }
+
+/**
+ * Edit a comment
+ */
+exports.update = function(req, res) {
+  res.send('Not IMPLEMENTED: Comment edit PUT')
 }
 
 /**
- * Get comment
- * @returns {Comment}
+ * Delete a comment
  */
-function get(req, res) {
-  return res.json(req.comment);
+exports.del = function(req, res) {
+  res.send('Not IMPLEMENTED: Comment delete DELETE')
 }
-
-/**
- * Create new comment
- * @property {string} req.body.username - The username of user.
- * @property {string} req.body.mobileNumber - The mobileNumber of user.
- * @returns {User}
- */
-function create(req, res, next) {
-  const { username, password, email } = req.body;
-  const saltRounds = 10;
-  bcrypt.hash(password, saltRounds)
-    .then(hash => new User({
-        username,
-        password: hash,
-        email
-      }))
-    .then(user => user.save())
-    .then(savedUser => res.json(savedUser))
-    .catch(e => next(e));
-}
-
-/**
- * Get user list.
- * @property {number} req.query.skip - Number of users to be skipped.
- * @property {number} req.query.limit - Limit number of users to be returned.
- * @returns {User[]}
- */
-function list(req, res, next) {
-  let { skip, limit } = req.query;
-  User.list({ skip, limit })
-    .then(data => res.json(data))
-    .catch(err => next(err))
-}
-
-export default { load, get, create, list };
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import userRoutes from './users.route';
 import postRoutes from './posts.route';
+import commentRoutes from './comments.route';
 
 const router = express.Router(); // eslint-disable-line new-cap
 
@@ -11,8 +12,22 @@ router.get('/health-check', (req, res) =>
 
 // mount user routes at /users
 router.use('/users', userRoutes);
+
 // mount post routes at /posts
 router.use('/posts', postRoutes);
+
+/** GET /api/comments	 - Get list of comments */
+router.get('/comments/', commentsController.index)
+
+/** POST /api/comments - Create new comment */
+router.post('/comments/', commentsController.create)
+
+/** PUT /api/comments:id - Update a comment */
+router.put('/comments/:id', commentsController.update)
+
+/** PUT /api/comments:id - Delete a comment */
+router.delete('/comments/:id', commentsController.del);
+
 
 // export default router;
 module.exports = router;
