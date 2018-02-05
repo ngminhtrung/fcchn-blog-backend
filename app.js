262cc3eb48a4ca0c4d975b25a7c355909931d6b3
tungvu3196 <tungvu3196@gmail.com>
2018-02-05 23:24:19 +0700

    Update logger.

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
+var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var routes = require('./routes');
 var mongoose = require('mongoose');
+var routes = require('./routes');
+var logger = require('./config/logger')
 
 var app = express();
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -22,7 +23,7 @@ app.use('/api', routes);
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
-  res.send(err);
+  next(err)
 });
 
 // error handler
@@ -30,6 +31,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+  logger.error(err.stack)
   res.status(err.status || 500);
   res.send(err); 
 });
@@ -39,8 +41,8 @@ mongoose.Promise = Promise;
 
 // connect to mongo db
 mongoose.connect('mongodb://localhost:27017/fcchn-blog').then(
-  () => { console.log('connect to mongo successfully'); },
-  err => { console.log('error on connect mongodb: ', err); }
+  () => { logger.info('connect to mongo successfully'); },
+  err => { logger.info('error on connect mongodb: ', err); }
 );
 
 module.exports = app;
