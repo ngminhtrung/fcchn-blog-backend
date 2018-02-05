69dc66373c3cb0cf8ca71390f2d511f044d29b85
tungvu3196 <tungvu3196@gmail.com>
2018-02-05 13:45:54 +0700

    remove console.log().

--- app.js
+++ app.js
@@ -31,7 +31,6 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   res.status(err.status || 500);
-  console.log(err);
   res.send(err); 
 });
 
