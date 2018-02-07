cc7ac57ce8d2db020ba0850e9929febc86ab4c86
Trung Nguyen <minhtrung.nguyen@live.com>
2018-02-07 14:01:54 +0700

    add unit test

---
+++ test/comments.test.js
@@ -0,0 +1,32 @@
+//During the test the env variable is set to test
+process.env.NODE_ENV = 'test';
+
+//Require the dev-dependencies
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let app = require('../app');
+let should = chai.should();
+
+chai.use(chaiHttp);
+//Our parent block
+describe('Comments', () => {
+    beforeEach((done) => {
+        //Before each test we empty the database in your case
+        done();
+    });
+    /*
+     * Test the /GET route
+     */
+    describe('/api/comments GET', () => {
+        it('it should GET all the comments', (done) => {
+            chai.request(app)
+                .get('/api/comments')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+                    res.body.length.should.be.eql(9); // fixme :)
+                    done();
+                });
+        });
+    });
+});
